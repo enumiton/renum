bcc300f52d278be781eb844fa3c3cfea1a4ba011
Enumiton <41439661+enumiton@users.noreply.github.com>
2022-06-03 23:37:54 +0200

    svg props

--- generate_icons.js
+++ generate_icons.js
@@ -42,7 +42,7 @@ async function generate() {
 		const svg = content
 			.replace(
 				/<svg .*>/,
-				'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true">'
+				'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">'
 			)
 			.replace(/(\n|\r|\t)+/g, '');
 
