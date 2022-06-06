e8c8627319f929898579f43e6572b55dedb1c3db
Enumiton <41439661+enumiton@users.noreply.github.com>
2022-06-06 16:57:56 +0200

    loading component preview

---
+++ src/components/loading/loading.preview.tsx
@@ -0,0 +1,14 @@
+import { Loading } from './loading';
+import './style/index.less';
+const config = {
+	title: 'loading',
+};
+
+function Example() {
+	return (
+		<Loading active />
+	);
+}
+
+export { Example };
+export default config;
