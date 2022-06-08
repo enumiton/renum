da7412895b850d7e075eb191af93686ab2f2f6ee
Enumiton <41439661+enumiton@users.noreply.github.com>
2022-06-08 09:27:58 +0200

    fix: missing export

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { Button } from './components/button';
+export type { ButtonProps, ButtonShape, ButtonType } from './components/button';
+
+export { Icon } from './components/icon';
+export type { IconProps } from './components/icon';
+
+export { Input } from './components/input';
+export type { InputProps } from './components/input';
+
+export { Loading } from './components/loading';
+export type { LoadingProps } from './components/loading';
+
+export { RenumProvider, RenumConsumer, useConfigProvider } from './components/renum-provider';
+export type { RenumConfig } from './components/renum-provider';
+
+export {
+	classNames,
+	isString,
+} from './utils';
